d0c21f1cb15eb89bbcd6cfb925bcae7dc9796072
Dri <drikill01@gmail.com>
2023-04-27 05:55:12 -0300

    Created a Class Seller

--- src/main/java/me/dri/aplication/Main.java
+++ src/main/java/me/dri/aplication/Main.java
@@ -1,6 +1,9 @@
 package me.dri.aplication;
 
 import me.dri.model.entities.Department;
+import me.dri.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
 
@@ -9,6 +12,10 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob" , "bob@gmail.com" , new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 
 }
